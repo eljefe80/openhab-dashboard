060c980cd9e017cec5ae059b8e17a4cb0af0516b
LambdaDriver <dsnigier@gmail.com>
2016-04-23 14:51:29 -0400

    added docker support

---
+++ docker/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+# pull a copy of dashing if it doesn't already exist
+if [[ ! -e /dashing/config.ru ]]; then
+  git clone 'https://github.com/LambdaDriver/openhab-dashboard.git' /dashing
+fi
+
+# install gems
+cd /dashing
+bundle install
+
+if [[ ! -z "$PORT" ]]; then
+  PORT_ARG="-p $PORT"
+fi
+
+# Start dashing
+exec dashing start $PORT_ARG
