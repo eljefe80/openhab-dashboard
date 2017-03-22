eaf36e5dce473a9afc55f0e39898ae2adfe4c723
jeff <jeff@wdwconsulting.net>
2017-03-22 06:11:42 -0700

    revert

--- jobs/cameras.rb
+++ jobs/cameras.rb
@@ -11,13 +11,6 @@ get '/cameras/refresh' do
   send_event(params['camera'], image: cam.make_web_friendly(new_file))
 end
 
-SCHEDULER.every '1h', first_in: 0 do
- files = Dir.foreach("assets/images/cameras/").select { |x| File.file?("assets/images/cameras/#{x}") }
- files.each do |file|
-#   path
-   FileUtils.mv("assets/images/cameras/" + file, "assets/images/cameras/new" + file)
- end
-end
  
  
 SCHEDULER.every FETCHNEWIMAGEEVERY, first_in: 0 do
