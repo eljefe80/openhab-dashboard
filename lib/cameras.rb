536e612a85facfd192a235d355c0319f42667230
jeff <jeff@wdwconsulting.net>
2017-04-05 06:45:24 -0700

    debug

--- lib/cameras.rb
+++ lib/cameras.rb
@@ -10,8 +10,8 @@ class Cameras
         'Username' => 'admin', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/img/snapshot.cgi",
-        'newFile' => "assets/images/cameras/snapshot1_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot1_old.jpeg",
+        'newFile' => "assets/images/cameras/Basement_ipcam_a.jpeg",
+        'oldFile' => "assets/images/cameras/Basement_ipcam_b.jpeg",
         'type' => 'http' },
 
       'QSEE' => {         'Host' => "10.200.1.225",  ## CHANGE
@@ -19,8 +19,8 @@ class Cameras
          'Username' => 'None', ## CHANGE
          'Password' => '', ## CHANGE
          'URL' => "/zm/cgi-bin/nph-zms?mode=single&monitor=7&scale=100&maxfps=1&buffer=1000",
-         'newFile' => "assets/images/cameras/snapshot3_new.jpeg",
-         'oldFile' => "assets/images/cameras/snapshot3_old.jpeg",
+         'newFile' => "assets/images/cameras/QSEE_a.jpeg",
+         'oldFile' => "assets/images/cameras/QSEE_b.jpeg",
         'type' => 'http' },
 
      'FrontPorch_FF_ipcam' => { 'Host' => "merilille",  ## CHANGE
@@ -28,16 +28,16 @@ class Cameras
         'Username' => 'None', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/zm/cgi-bin/nph-zms?mode=single&monitor=1&scale=100&maxfps=1&buffer=1000",
-        'newFile' => "assets/images/cameras/snapshot3_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot3_old.jpeg",
+        'newFile' => "assets/images/cameras/FrontPorch_FF_ipcam_a.jpeg",
+        'oldFile' => "assets/images/cameras/FrontPorch_FF_ipcam_b.jpg",
         'type' => 'http' },
      'Frontporch2' => { 'Host' => "10.200.1.225",  ## CHANGE
         'Port' => "443",  ## CHANGE
         'Username' => 'None', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/zm/cgi-bin/nph-zms?mode=single&monitor=8&scale=100&maxfps=1&buffer=1000",
-        'newFile' => "assets/images/cameras/snapshot3_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot3_old.jpeg",
+        'newFile' => "assets/images/cameras/Frontporch2_a.jpeg",
+        'oldFile' => "assets/images/cameras/Frontporch2_b.jpeg",
         'type' => 'http' },
 
      'blink' => { 'Host' => "10.200.1.225",  ## CHANGE
@@ -45,8 +45,8 @@ class Cameras
         'Username' => 'None', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/zm/cgi-bin/nph-zms?mode=single&monitor=10&scale=100&maxfps=1&buffer=1000",
-        'newFile' => "assets/images/cameras/snapshot3_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot3_old.jpeg",
+        'newFile' => "assets/images/cameras/blink_a.jpeg",
+        'oldFile' => "assets/images/cameras/blink_b.jpeg",
         'type' => 'http' },
 
      'BackPorch_FF_ipcam' => { 'Host' => "merilille",  ## CHANGE
@@ -54,8 +54,8 @@ class Cameras
         'Username' => 'None', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/zm/cgi-bin/nph-zms?mode=single&monitor=2&scale=100&maxfps=1&buffer=1000",
-        'newFile' => "assets/images/cameras/snapshot4_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot4_old.jpeg",
+        'newFile' => "assets/images/cameras/BackPorch_FF_ipcam_a.jpeg",
+        'oldFile' => "assets/images/cameras/BackPorch_FF_ipcam_b.jpeg",
         'type' => 'http' },
 
      'Garage_ipcam' => { 'Host' => "merilille",  ## CHANGE
@@ -63,8 +63,8 @@ class Cameras
         'Username' => 'None', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/zm/cgi-bin/nph-zms?mode=single&monitor=3&scale=100&maxfps=1&buffer=1000",
-        'newFile' => "assets/images/cameras/snapshot4_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot4_old.jpeg",
+        'newFile' => "assets/images/cameras/Garage_ipcam_a.jpeg",
+        'oldFile' => "assets/images/cameras/Garage_ipcam_b.jpeg",
         'type' => 'http' },
 
       'cubie' => {         'Host' => "cubieboard",  ## CHANGE
@@ -72,16 +72,16 @@ class Cameras
          'Username' => 'admin', ## CHANGE
          'Password' => 'admin', ## CHANGE
          'URL' => "/cgi-bin/nph-mjgrab?1",
-         'newFile' => "assets/images/cameras/snapshot3_new.jpeg",
-         'oldFile' => "assets/images/cameras/snapshot3_old.jpeg",
+         'newFile' => "assets/images/cameras/cubie_a.jpeg",
+         'oldFile' => "assets/images/cameras/cubie_b.jpeg",
         'type' => 'http' },
      'Frontroom_FF_ipcam' => { 'Host' => "SC5CEB79",  ## CHANGE
         'Port' => "80",  ## CHANGE
         'Username' => 'None', ## CHANGE
         'Password' => '', ## CHANGE
         'URL' => "/img/snapshot.cgi",
-        'newFile' => "assets/images/cameras/snapshot2_new.jpeg",
-        'oldFile' => "assets/images/cameras/snapshot2_old.jpeg",
+        'newFile' => "assets/images/cameras/Frontroom_FF_ipcam_a.jpeg",
+        'oldFile' => "assets/images/cameras/Frontroom_FF_ipcam_b.jpeg",
         'type' => 'http' },
 
       'foscam' => {         'Host' => "10.200.1.63",  ## CHANGE
@@ -89,8 +89,8 @@ class Cameras
          'Username' => 'admin', ## CHANGE
          'Password' => 'lokimoki', ## CHANGE
          'URL' => "/snapshot.cgi?user=admin",
-         'newFile' => "assets/images/cameras/snapshot3_new.jpeg",
-         'oldFile' => "assets/images/cameras/snapshot3_old.jpeg",
+         'newFile' => "assets/images/cameras/foscam_a.jpeg",
+         'oldFile' => "assets/images/cameras/foscam_b.jpeg",
         'type' => 'http' },
 
 #      'blink' => {         'Host' => "10.200.1.137",  ## CHANGE
@@ -140,7 +140,7 @@ class Cameras
   def fetch_image(cam)
 
 #	puts 'DEBUG '+cam
-	old_file = "assets/images/cameras/"+cam+"_old.jpg"
+	old_file = @@camera[cam]['oldFile']
 #        `pwd`
 #        `ls -la #{old_file}`
 #	`rm #{old_file}` 
@@ -164,8 +164,8 @@ class Cameras
                     if File.exist?(@@camera[cam]['newFile'])
                        FileUtils.mv(@@camera[cam]['newFile'],old_file)
                     end
-                    @@camera[cam]['oldFile'] = "assets/images/cameras/"+cam+"_old.jpg"
-                    @@camera[cam]['newFile'] = "assets/images/cameras/"+cam+".jpg"
+                    @@camera[cam]['oldFile'] = @@camera[cam]['newFile']
+                    @@camera[cam]['newFile'] = old_file
                     open(@@camera[cam]['newFile'], "wb") do |file|
 			file.write(response.body)
                     end
